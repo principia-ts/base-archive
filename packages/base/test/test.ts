af96028147ff0cef0c586e44b676cb8dc7c5b8be
0x706b <peter@peterkrol.com>
2020-12-28 16:31:54 -0500

    Try zio Stack implementation for FiberContext

--- packages/base/test/test.ts
+++ packages/base/test/test.ts
@@ -1,3 +1,16 @@
 import "../src/unsafe/Operators";
 
-import * as A from "../src/data/Array";
+import * as Eval from "../src/control/Eval";
+
+const factorial = (n: number): Eval.Eval<number> =>
+  Eval.gen(function* (_) {
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * (yield* _(factorial(n - 1)));
+    }
+  });
+
+console.time("A");
+factorial(100)["|>"](Eval.evaluate)["|>"](console.log);
+console.timeEnd("A");
