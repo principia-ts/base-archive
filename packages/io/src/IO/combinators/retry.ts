db100d90a3134f1002fc880697962af64e832a70
0x706b <peter@peterkrol.com>
2021-03-08 19:23:53 -0500

    io/IO: Improve retry types

--- packages/io/src/IO/combinators/retry.ts
+++ packages/io/src/IO/combinators/retry.ts
@@ -14,7 +14,10 @@ import { bind, catchAll, fail, map, map_, matchM, orDie } from '../core'
  * `once` or `recurs` for example), so that that `io.retry(Schedule.once)` means
  * "execute `io` and in case of failure, try again once".
  */
-export function retry_<R, E, A, R1, O>(fa: IO<R, E, A>, policy: S.Schedule<R1, E, O>): IO<R & R1 & Has<Clock>, E, A> {
+export function retry_<R, E extends I, A, R1, I, O>(
+  fa: IO<R, E, A>,
+  policy: S.Schedule<R1, I, O>
+): IO<R & R1 & Has<Clock>, E, A> {
   return retryOrElse_(fa, policy, (e, _) => fail(e))
 }
 
@@ -24,8 +27,10 @@ export function retry_<R, E, A, R1, O>(fa: IO<R, E, A>, policy: S.Schedule<R1, E
  * `once` or `recurs` for example), so that that `io.retry(Schedule.once)` means
  * "execute `io` and in case of failure, try again once".
  */
-export function retry<R1, E, O>(policy: S.Schedule<R1, E, O>) {
-  return <R, A>(fa: IO<R, E, A>): IO<R & R1 & Has<Clock>, E, A> => retry_(fa, policy)
+export function retry<R1, I, O>(
+  policy: S.Schedule<R1, I, O>
+): <R, E extends I, A>(fa: IO<R, E, A>) => IO<R & R1 & Has<Clock>, E, A> {
+  return (fa) => retry_(fa, policy)
 }
 
 /**
@@ -33,9 +38,9 @@ export function retry<R1, E, O>(policy: S.Schedule<R1, E, O>) {
  * value produced by the schedule together with the last error are passed to
  * the recovery function.
  */
-export function retryOrElse_<R, E, A, R1, O, R2, E2, A2>(
+export function retryOrElse_<R, E extends I, A, R1, I, O, R2, E2, A2>(
   fa: IO<R, E, A>,
-  policy: S.Schedule<R1, E, O>,
+  policy: S.Schedule<R1, I, O>,
   orElse: (e: E, o: O) => IO<R2, E2, A2>
 ): IO<R & R1 & R2 & Has<Clock>, E2, A | A2> {
   return map_(retryOrElseEither_(fa, policy, orElse), E.merge)
@@ -46,17 +51,17 @@ export function retryOrElse_<R, E, A, R1, O, R2, E2, A2>(
  * value produced by the schedule together with the last error are passed to
  * the recovery function.
  */
-export function retryOrElse<E, R1, O, R2, E2, A2>(
-  policy: S.Schedule<R1, E, O>,
+export function retryOrElse<E extends I, R1, I, O, R2, E2, A2>(
+  policy: S.Schedule<R1, I, O>,
   orElse: (e: E, o: O) => IO<R2, E2, A2>
 ) {
   return <R, A>(fa: IO<R, E, A>) => retryOrElse_(fa, policy, orElse)
 }
 
-const _loop = <R, E, A, R1, O, R2, E2, A2>(
+const _loop = <R, E extends I, A, R1, I, O, R2, E2, A2>(
   fa: IO<R, E, A>,
   orElse: (e: E, o: O) => IO<R2, E2, A2>,
-  driver: S.Driver<R1 & Has<Clock>, E, O>
+  driver: S.Driver<R1 & Has<Clock>, I, O>
 ): IO<R & R1 & R2 & Has<Clock>, E2, E.Either<A2, A>> =>
   pipe(
     fa,
@@ -82,9 +87,9 @@ const _loop = <R, E, A, R1, O, R2, E2, A2>(
  * the schedule is done, then both the value produced by the schedule together with the last
  * error are passed to the specified recovery function.
  */
-export function retryOrElseEither_<R, E, A, R1, O, R2, E2, A2>(
+export function retryOrElseEither_<R, E extends I, A, R1, I, O, R2, E2, A2>(
   fa: IO<R, E, A>,
-  policy: S.Schedule<R1, E, O>,
+  policy: S.Schedule<R1, I, O>,
   orElse: (e: E, o: O) => IO<R2, E2, A2>
 ): IO<R & R1 & R2 & Has<Clock>, E2, E.Either<A2, A>> {
   return pipe(
@@ -99,8 +104,8 @@ export function retryOrElseEither_<R, E, A, R1, O, R2, E2, A2>(
  * the schedule is done, then both the value produced by the schedule together with the last
  * error are passed to the specified recovery function.
  */
-export function retryOrElseEither<E, R1, O, R2, E2, A2>(
-  policy: S.Schedule<R1, E, O>,
+export function retryOrElseEither<E extends I, R1, I, O, R2, E2, A2>(
+  policy: S.Schedule<R1, I, O>,
   orElse: (e: E, o: O) => IO<R2, E2, A2>
 ) {
   return <R, A>(fa: IO<R, E, A>) => retryOrElseEither_(fa, policy, orElse)
