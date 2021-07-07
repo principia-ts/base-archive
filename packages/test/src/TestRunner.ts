7bba9142474d935c0624c2bcbf76a3f5f50bfcd2
0x706b <peter@peterkrol.com>
2021-07-07 19:36:48 -0400

    fix: bring up to date with base

--- packages/test/src/TestRunner.ts
+++ packages/test/src/TestRunner.ts
@@ -3,16 +3,18 @@ import type { TestReporter } from './api'
 import type { ExecutedSpec } from './ExecutedSpec'
 import type { XSpec } from './Spec'
 import type { TestExecutor } from './TestExecutor'
+import type { Clock } from '@principia/base/Clock'
 import type { Platform } from '@principia/base/Fiber'
 import type { Has } from '@principia/base/Has'
 import type { URIO } from '@principia/base/IO'
 import type { Layer } from '@principia/base/Layer'
 
-import { Clock } from '@principia/base/Clock'
-import { Console } from '@principia/base/Console'
+import { ClockTag, LiveClock } from '@principia/base/Clock'
+import { ConsoleTag, LiveConsole } from '@principia/base/Console'
 import { parallelN } from '@principia/base/ExecutionStrategy'
 import { pipe } from '@principia/base/function'
 import * as I from '@principia/base/IO'
+import * as L from '@principia/base/Layer'
 
 import { defaultTestAnnotationRenderer, report } from './Render'
 import { TestLogger } from './TestLogger'
@@ -22,9 +24,9 @@ export class TestRunner<R, E> {
     readonly executor: TestExecutor<R>,
     readonly platform: Platform<unknown> = I.defaultRuntime.platform,
     readonly reporter: TestReporter<E> = report(defaultTestAnnotationRenderer),
-    readonly bootstrap: Layer<unknown, never, Has<TestLogger> & Has<Clock>> = Console.live['>=>'](
-      TestLogger.fromConsole
-    )['+++'](Clock.live)
+    readonly bootstrap: Layer<unknown, never, Has<TestLogger> & Has<Clock>> = L.succeed(ConsoleTag)(new LiveConsole())
+      ['>=>'](TestLogger.fromConsole)
+      ['+++'](L.succeed(ClockTag)(new LiveClock()))
   ) {
     this.run = this.run.bind(this)
   }
